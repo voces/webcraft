200c5a9b0dfc1a23d43c9a1235ddfe900bc32440
Robert Coe <chakrawc@gmail.com>
2015-01-01 14:42:53 -0800

    Add/remove sections within code
    Stuff works...
    Realized I need to change how .code works. It should be an array of objects, each object specified as such: {name: 'VALUE', code: 'VALUE', children: []} (children can be left out if empty, I guess)

--- editor/code/logic.js
+++ editor/code/logic.js
@@ -148,18 +148,34 @@ logic.loadSection = function(id, value, code, parent) {
 	label.className = 'caret';
 	
 	var text = document.createElement('span');
+	text.className = 'sectionName';
 	text.innerText = value;
+	
+	var controls = document.createElement('span');
+	controls.className = 'controls';
+	
+	var add = document.createElement('span');
+	add.className = 'add';
+	add.innerText = '+';
+	
+	var remove = document.createElement('span');
+	remove.className = 'remove';
+	remove.innerText = '–';
+	
+	controls.appendChild(add);
+	controls.appendChild(remove);
 	
 	li.appendChild(input);
 	li.appendChild(label);
 	li.appendChild(text);
+	li.appendChild(controls);
 	
 	//Not a mod-level section, so we're adding to a list
   if (parent) {
 		
 		//Grab the parent and the list
 		parentEle = document.getElementById(parent).parentNode;
-		var childList = parentEle.children[3];
+		var childList = parentEle.children[4];
 		
 		//List doesn't exist yet so create it
 		if (!childList) {
@@ -241,7 +257,7 @@ logic.rFinishRename = function(li, oldName, newName) {
 			li.children[1].getAttribute('for').replace(oldName, newName));
 	
 	//And update any sublists
-	var next = li.children[3];
+	var next = li.children[4];
 	if (next)
 		for (var i = 0, child; child = next.children[i]; i++)
 			this.rFinishRename(child, oldName, newName);
@@ -288,7 +304,7 @@ logic.finishRename = function(e) {
  */
 logic.renameSection = function(e) {
   
-	if (e.target.tagName == 'SPAN' && e.target.className != 'caret' &&
+	if (e.target.className == 'sectionName' &&
 			e.target.parentNode.children[0].id.indexOf('_') > 0) {
 		
 		e.target.oldText = e.target.innerText;
@@ -298,39 +314,6 @@ logic.renameSection = function(e) {
 	}
 	
 };
- 
-logic.newSection = function() {
-	var parentId = this.tree.getSelectedId();
-	var parent = this.tree.getSelectedItem();
-	
-	if (!parent) {
-		message({
-			error: 'true',
-			text: 'You must select a mod to add a selection to.'
-		});
-		
-		return;
-	}
-	
-  var section = this.idToCode(parentId);
-  var value = 'Untitled';
-  
-  if (typeof section[value] != 'undefined') {
-    var num = 2;
-    
-    while (typeof section[value + ' ' + num] != 'undefined')
-      num++;
-    
-    value = value + ' ' + num;
-  }
-  
-	section[value] = {_value: ''};
-  
-	this.tree.add({
-		id: parentId + "_" + value,
-		value: value
-	}, null, parentId);
-};
 
 logic.onChange = function(e, section) {
   section._value = this.coder.getValue();
@@ -338,29 +321,87 @@ logic.onChange = function(e, section) {
 
 logic.selectSection = function(e) {
 	
-	//Ignore the span event
-	if (e.target.tagName != 'SPAN') return;
+	//Section name selected, change code sessions
+	if (e.target.className == 'sectionName') {
+		
+		//Get the id and update currentMod
+		var id = e.target.parentNode.children[0].id;
+		this.currentMod = id.split('_')[0].substr(1);
+		
+		//Grab the section
+		var section = this.idToCode(id);
+		
+		//Create a session if it does not exist
+		if (typeof section._session == "undefined") {
+			section._session = ace.createEditSession(
+				section._value, 'ace/mode/javascript'
+			);
+			
+			section._session.on('change', function(e) {
+				this.onChange(e, section);
+			}.bind(this));
+		}
+		
+		//Set the session
+		this.coder.setSession(section._session);
 	
-	//Get the id and update currentMod
-	var id = e.target.parentNode.children[0].id;
-	this.currentMod = id.split('_')[0].substr(1);
+	//New section clicked
+	} else if (e.target.className == 'add') {
+		
+		//Grab the id
+		var currentId = e.target.parentNode.parentNode.children[0].id;
+		
+		//Grab the section and define our first-pass section name
+		var section = this.idToCode(currentId);
+		var value = 'Untitled',
+				num = '';
+		
+		//Loop until we have a unique section
+		while (typeof section[value + num] != 'undefined')
+			num = (parseInt(num) || 1) + 1;
+		
+		value += num;
+		
+		//Define the new section in code
+		section[value] = {_value: ''};
+		
+		//Add the new section to the tree
+		this.loadSection(currentId + '_' + value, value, null, currentId);
+		
+		//Show the section if it's not
+		e.target.parentNode.parentNode.children[0].checked = true
 	
-	//Grab the section
-  var section = this.idToCode(id);
-	
-	//Create a session if it does not exist
-  if (typeof section._session == "undefined") {
-    section._session = ace.createEditSession(
-      section._value, 'ace/mode/javascript'
-    );
-    
-    section._session.on('change', function(e) {
-      this.onChange(e, section);
-    }.bind(this));
-  }
-  
-  //Set the session
-  this.coder.setSession(section._session);
+	//Remove section clicked
+	} else if (e.target.className == 'remove') {
+		
+		//Grab info about the current item
+		var curId = e.target.parentNode.parentNode.children[0].id;
+		var curItem = e.target.parentNode.parentNode;
+		var listNode = curItem.parentNode;
+		
+		//Verify they want to delete
+		if (prompt('Are you sure you want to delete ' +
+				curItem.children[2].innerText + ' (type "yes")?') != 'yes')
+			return;
+		
+		//Get the id of parent
+		var parentId = curId.substr(0, curId.lastIndexOf('_'));
+		var curName = curId.substr(curId.lastIndexOf('_') + 1);
+		
+		//Now grab the section of code
+		var parentSection = this.idToCode(parentId);
+		
+		//Remove in code
+		delete parentSection[curName];
+		
+		//Remove in HTML
+		curItem.remove();
+		
+		//Hide the caret if now empty
+		if (listNode.children.length == 0)
+			listNode.parentNode.children[1].style.opacity = 0;
+		
+	}
 	
 };
 
@@ -391,10 +432,6 @@ logic.menuSwitch = function(e) {
 			this.saveMethod.nodeValue = 'Save local ';
 			this.saveLocal();
 			break;
-		
-		//Edit
-		case 'Rename section': this.renameSection(); break;
-		case 'New section': this.newSection(); break;
 		
 		//window
 		case 'Terrain Editor': window.open('..'); break;
