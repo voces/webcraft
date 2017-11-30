b7a4f768242b45f4e320ce5c603e752da542c75b
Robert Coe <chakrawc@gmail.com>
2017-11-29 18:55:53 -0800

    Hanging example tests
    - Each example test will kill new handles at the end of their test
    - Removed rollup build scripts

--- test/examples.test.js
+++ test/examples.test.js
@@ -7,11 +7,19 @@ export default () => describe( "examples", () => {
 
 			this.timeout( 750 );
 
+			const goodHandles = process._getActiveHandles();
+
 			import( `../examples/games/${example}/main.js` ).then( imported => {
 
 				imported.default.network.addEventListener( "ready", () => {
 
-					imported.default.network.ws.close();
+					process._getActiveHandles().filter( handle => ! goodHandles.includes( handle ) ).forEach( handle =>{
+
+						if ( typeof handle.close === "function" ) return handle.close();
+						if ( typeof handle.destroy === "function" ) return handle.destroy();
+
+					} );
+
 					done();
 
 				} );
