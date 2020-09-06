e15075f3b1cc8f42f81af0f85a48755139465b7c
Robert Coe <chakrawc@gmail.com>
2020-09-06 16:20:18 -0700

    one more....

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-	input: ["src/index.ts", "src/Game.ts", "src/Network.ts"],
+	input: ["src/index.ts", "src/Game.ts", "src/network.ts"],
 	output: [{ dir: "public/js", format: "es" }],
 	plugins: [
 		typescript({
