06fbe631284c7d8c77eb88917b9fb837566e7ddc
Robert Coe <chakrawc@gmail.com>
2020-09-13 18:13:08 -0700

    GameType fixes (#41)

--- src/ui/preact/contexts/Game.ts
+++ src/ui/preact/contexts/Game.ts
@@ -2,14 +2,15 @@ import { createContext } from "preact";
 import { App } from "../../../core/App";
 import { Emitter } from "../../../emitter";
 import { Game as WCGame, GameEvents } from "../../../Game";
+import { Round } from "../../../Round";
 
 type GameType = Emitter<GameEvents> &
 	App & {
 		addNetworkListener: WCGame["addNetworkListener"];
+		connect: WCGame["connect"];
+		localPlayer?: WCGame["localPlayer"];
 		removeNetworkListener: WCGame["removeNetworkListener"];
-		round?: {
-			expireAt: number;
-		};
+		round?: { expireAt: Round["expireAt"] };
 	};
 
 // While this can be dangerous, we're going to assume a context as always been
