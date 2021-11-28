4574cb90bf649fb294f7d0325535575e674102e7
Robert Coe <chakrawc@gmail.com>
2021-11-28 09:52:29 -0800

    drop date on logLine

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,12 +1,12 @@
 import { currentApp } from "./appContext";
 
 export const logLine = (...data: unknown[]): void => {
-	let time;
 	try {
-		time = currentApp().time;
+		const time = currentApp().time;
+		if (time) data.unshift(time);
 	} catch {
 		/* do nothing */
 	}
 	// eslint-disable-next-line no-console
-	console.log(new Date(), time, ...data);
+	console.log(...data);
 };
